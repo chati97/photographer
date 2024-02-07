95e6bfa0fe70b69916cf2a1209750e24b6996a17
mji4209 <mji4209@naver.com>
2024-02-07 20:35:59 +0900

    [folder] 폴더정리

---
+++ front-end/photobychati/src/component/Body.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      body
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/photobychati/src/component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tmp() {
+    return (
+        <div>
+            footer
+        </div>
+    );
+}
+
+export default Tmp
