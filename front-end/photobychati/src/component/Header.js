d37b66992f047e28477a5ab2fb3488f3e640adb3
mji4209 <mji4209@naver.com>
2024-02-07 21:32:59 +0900

    [style] 네비게이션바 배치

--- front-end/photobychati/src/component/Header.js
+++ front-end/photobychati/src/component/Header.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import '../css/navbar.css';
+import logo from '../img/logo.png';
 
 function Header() {
     return (
         <div>
-            <a href='/main'>main</a>
-            <a href='/info'>info</a>
-            <a href='/gallery'>gallery</a>
-            <a href='/contact'>contact</a>
+            <div className="logo">
+                <a href='/'>
+                    <img src={logo}></img>
+                </a>
+            </div>
+            <div id="navbar">
+                <a href='/'>main</a>
+                <a href='/info'>info</a>
+                <a href='/gallery'>gallery</a>
+                <a href='/contact'>contact</a>
+            </div>
         </div>
     )
 }
--- front-end/photobychati/src/page/MainPage.js
+++ front-end/photobychati/src/page/MainPage.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div
+      style={{backgroundColor: 'gray', height: '100vh'}}
+    >MainPage</div>
   )
 }
 
